cc90d723f4c5abbae4631e148c42c06f332a275d
fiosman <fares.osman@outlook.com>
2022-05-24 23:27:36 -0400

    Adds trees table

---
+++ server/migrations/20220525032713-create-tree.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Trees', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      tree: {
+        type: Sequelize.STRING
+      },
+      location: {
+        type: Sequelize.STRING
+      },
+      height_ft: {
+        type: Sequelize.FLOAT
+      },
+      ground_circumference_ft: {
+        type: Sequelize.FLOAT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Trees');
+  }
+};
