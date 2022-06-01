8701a879aea6083d882c10374b62c7b84f536a24
fiosman <fares.osman@outlook.com>
2022-05-31 22:08:13 -0400

    get started on insect constraints

--- server/migrations/20220531030815-create-insect.js
+++ server/migrations/20220531030815-create-insect.js
@@ -1,39 +1,41 @@
-'use strict';
+"use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Insects', {
+    await queryInterface.createTable("Insects", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       description: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       territory: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       fact: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       millimeters: {
-        type: Sequelize.FLOAT
+        type: Sequelize.FLOAT,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Insects');
-  }
+    await queryInterface.dropTable("Insects");
+  },
 };
